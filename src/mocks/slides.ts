1ce7cf85c0131901874bcdcfaf01647f0ccf9ee0
Stone C. Lasley <stonelasley@gmail.com>
2017-04-06 21:58:03 -0600

    Slides, Toast

---
+++ src/mocks/slides.ts
@@ -0,0 +1,35 @@
+import { Slides }   from 'ionic-angular';
+
+export class SlidesMock {
+	public static instance(): Slides {
+
+		let instance = jasmine.createSpyObj('Slides', [
+			'controlBy',
+			'controlInverse',
+			'enableKeyboardControl',
+			'getActiveIndex',
+			'getPreviousIndex',
+			'isBeginning',
+			'isEnd',
+			'length',
+			'lockSwipeToNext',
+			'lockSwipeToPrev',
+			'lockSwipes',
+			'resize',
+			'slideNext',
+			'slidePrev',
+			'slideTo',
+			'startAutoplay',
+			'stopAutoplay',
+			'update'
+		]);
+
+		instance.getActiveIndex.and.returnValue(0);
+		instance.getPreviousIndex.and.returnValue(0);
+		instance.isBeginning.and.returnValue(true);
+		instance.isEnd.and.returnValue(false);
+		instance.length.and.returnValue(3);
+
+		return instance;
+	}
+}
