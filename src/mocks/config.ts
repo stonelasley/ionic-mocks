c8545ec96c7480b0ef8931c17bccc33706f3bb9d
Stone C. Lasley <stonelasley@gmail.com>
2017-07-04 12:07:09 -0600

    fix(config): add missing methods

--- src/mocks/config.ts
+++ src/mocks/config.ts
@@ -1,6 +1,16 @@
 export class ConfigMock {
 	public static instance(): any {
-		let instance = jasmine.createSpyObj('Config', ['get', 'getBoolean', 'getNumber', 'setTransition']);
+		let instance = jasmine.createSpyObj('Config', [
+			'get',
+			'getBoolean',
+			'getNumber',
+			'set',
+			'settings',
+			'setModeConfig',
+			'getModeConfig',
+			'setTransition',
+			'getTransition'
+		]);
 
 		instance.get.and.returnValue('');
 		instance.getBoolean.and.returnValue(true);
