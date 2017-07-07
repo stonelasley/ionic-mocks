31b5d1f94f100071780a805dbc7a8f37c46871c5
Stone C. Lasley <stonelasley@gmail.com>
2017-07-06 21:47:03 -0600

    feat: add open and close to menu

--- src/mocks/menu.ts
+++ src/mocks/menu.ts
@@ -1,14 +1,16 @@
 export class MenuMock {
 	public static instance(): any {
-		let instance = jasmine.createSpyObj('Menu', ['blank']);
-		instance['content'] = "menu content";
+		let instance = jasmine.createSpyObj('Menu', ['blank', 'open', 'close']);
+		instance['content'] = 'menu content';
 		instance['enabled'] = true;
 		instance['id'] = 'menuId';
 		instance['persistent'] = true;
-		instance['side'] = "left";
+		instance['side'] = 'left';
 		instance['swipeEnabled'] = true;
-		instance['type'] = "reveal";
+		instance['type'] = 'reveal';
 
+		instance.open.and.returnValue(Promise.resolve(true));
+		instance.close.and.returnValue(Promise.resolve(true));
 
 		return instance;
 	}
