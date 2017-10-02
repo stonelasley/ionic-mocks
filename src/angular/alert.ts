8a24427bbff1ac918e692e620cb92b49cf178765
Andrew <block159@gmail.com>
2017-10-02 11:01:33 +0200

    fire onDidDismiss callback for alert and modal

--- src/angular/alert.ts
+++ src/angular/alert.ts
@@ -1,8 +1,17 @@
 export class AlertMock {
     public static instance(): any {
-        let instance = jasmine.createSpyObj('Alert', ['present', 'dismiss']);
+    	let _dismissCallback: Function;
+        let instance = jasmine.createSpyObj('Alert', ['present', 'dismiss', 'onDidDismiss']);
         instance.present.and.returnValue(Promise.resolve());
-        instance.dismiss.and.returnValue(Promise.resolve());
+
+        instance.dismiss.and.callFake(() => {
+            _dismissCallback();
+            return Promise.resolve();
+        });
+
+        instance.onDidDismiss.and.callFake((callback: Function) => {
+            _dismissCallback = callback;
+        });
 
         return instance;
     }
--- src/angular/modal.ts
+++ src/angular/modal.ts
@@ -1,8 +1,17 @@
 export class ModalMock {
     public static instance(): any {
+    	let _dismissCallback: Function;
         let instance = jasmine.createSpyObj('Modal', ['present', 'dismiss', 'onDidDismiss']);
         instance.present.and.returnValue(Promise.resolve());
-        instance.dismiss.and.returnValue(Promise.resolve());
+        
+        instance.dismiss.and.callFake(() => {
+            _dismissCallback();
+            return Promise.resolve();
+        });
+
+        instance.onDidDismiss.and.callFake((callback: Function) => {
+            _dismissCallback = callback;
+        });
 
         return instance;
     }
