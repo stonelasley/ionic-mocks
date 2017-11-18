0759ab104d9e3f49d36397bbc10d6911fc5a4643
Matheus Valiente Souza <mvs@mvsouza.com>
2017-11-18 03:34:56 -0200

    fix(MenuController): Ajusted function 'swipeEnable' (#37)
    @mvsouza thank you.

--- src/angular/menu-controller.ts
+++ src/angular/menu-controller.ts
@@ -3,7 +3,7 @@ import {MenuMock} from './menu';
 export class MenuControllerMock {
     public static instance(menu?: MenuMock): any {
         let m = menu || MenuMock.instance();
-        let instance = jasmine.createSpyObj('MenuController', ['close', 'enable', 'get', 'getMenus', 'getOpen', 'isEnabled', 'isOpen', 'open', 'swipeEnabled', 'toggle']);
+        let instance = jasmine.createSpyObj('MenuController', ['close', 'enable', 'get', 'getMenus', 'getOpen', 'isEnabled', 'isOpen', 'open', 'swipeEnable', 'toggle']);
         instance.close.and.returnValue(Promise.resolve());
         instance.enable.and.returnValue(m);
         instance.get.and.returnValue(m);
@@ -12,7 +12,7 @@ export class MenuControllerMock {
         instance.isEnabled.and.returnValue(true);
         instance.isOpen.and.returnValue(false);
         instance.open.and.returnValue(Promise.resolve());
-        instance.swipeEnabled.and.returnValue(m);
+        instance.swipeEnable.and.returnValue(m);
         instance.toggle.and.returnValue(Promise.resolve());
 
         return instance;
