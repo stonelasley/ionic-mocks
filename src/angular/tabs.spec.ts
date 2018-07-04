cc7cc8ee03331c0b638ac00381c303f861ce9e93
Stone C. Lasley <stonelasley@gmail.com>
2018-07-04 10:32:34 -0600

    refactor(Tabs): Deprecate static instance method

---
+++ src/angular/tabs.spec.ts
@@ -0,0 +1,83 @@
+import { TabsMock } from './tabs';
+import { TabMock } from './tab';
+
+describe('Tabs', () => {
+    let classUnderTest: any;
+
+    beforeEach(() => {
+        classUnderTest = new TabsMock();
+    });
+
+    it('should be defined', () => {
+        expect(classUnderTest).toBeDefined();
+    });
+
+    describe('select', () => {
+        it('should be defined', () => {
+            expect(classUnderTest.select).toBeDefined();
+        });
+
+        it('should return Tab', () => {
+            expect(classUnderTest.select()).toEqual(jasmine.any(Object));
+        });
+
+        it('should return provided Tab', () => {
+            const tab = TabMock.instance();
+            classUnderTest = new TabsMock(tab);
+
+            const result = classUnderTest.select();
+
+            expect(result).toBe(tab);
+        });
+    });
+
+    describe('getSelected', () => {
+        it('should be defined', () => {
+            expect(classUnderTest.getSelected).toBeDefined();
+        });
+
+        it('should return Tab', () => {
+            expect(classUnderTest.getSelected()).toEqual(jasmine.any(Object));
+        });
+
+        it('should return provided Tab', () => {
+            const tab = TabMock.instance();
+            classUnderTest = new TabsMock(null, tab);
+
+            const result = classUnderTest.getSelected();
+
+            expect(result).toBe(tab);
+        });
+    });
+
+    describe('previousTab', () => {
+        it('should be defined', () => {
+            expect(classUnderTest.previousTab).toBeDefined();
+        });
+
+        it('should return Tab', () => {
+            expect(classUnderTest.previousTab()).toEqual(jasmine.any(Object));
+        });
+
+        it('should return provided Tab', () => {
+            const tab = TabMock.instance();
+            classUnderTest = new TabsMock(null, null, tab);
+
+            const result = classUnderTest.previousTab();
+
+            expect(result).toBe(tab);
+        });
+    });
+
+    describe('viewCtrl', () => {
+        it('should be defined', () => {
+            expect(classUnderTest.viewCtrl).toBeDefined();
+        });
+    });
+
+    describe('ngOnDestroy', () => {
+        it('should be defined', () => {
+            expect(classUnderTest.ngOnDestroy).toBeDefined();
+        });
+    });
+}); 
--- src/angular/tabs.ts
+++ src/angular/tabs.ts
@@ -1,19 +1,20 @@
-import {TabMock} from './tab';
+import { TabMock } from './tab';
+import { BaseMock } from '../base.mock';
+import { deprecated } from 'deprecated-decorator';
 
-export class TabsMock {
+const METHODS = ['select', 'getSelected', 'ngOnDestroy', 'previousTab', 'viewCtrl'];
+
+export class TabsMock extends BaseMock {
+
+    constructor(defaultTab?: TabMock, selectedTab?: TabMock, previousTab?: TabMock) {
+        super('Tabs', METHODS);
+        this.spyObj.select.and.returnValue(defaultTab || TabMock.instance());
+        this.spyObj.getSelected.and.returnValue(selectedTab || TabMock.instance());
+        this.spyObj.previousTab.and.returnValue(previousTab || TabMock.instance());
+    }
+
+    @deprecated('new TabsMock()')
     public static instance(defaultTab?: TabMock, selectedTab?: TabMock, previousTab?: TabMock): any {
-        let instance = jasmine.createSpyObj('Tabs', [
-            'select',
-            'getSelected',
-            'ngOnDestroy',
-            'previousTab',
-            'viewCtrl'
-        ]);
-
-        instance.select.and.returnValue(defaultTab || TabMock.instance());
-        instance.getSelected.and.returnValue(selectedTab || TabMock.instance());
-        instance.previousTab.and.returnValue(previousTab || TabMock.instance());
-
-        return instance;
+        return new TabsMock(defaultTab, selectedTab, previousTab);
     }
 }
