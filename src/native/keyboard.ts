6b8be727e9b7b2676243c4f2418ad95ba655cef2
Stone C. Lasley <stonelasley@gmail.com>
2018-07-06 23:55:19 -0600

    chore(Keyboard): Linting

--- src/native/keyboard.ts
+++ src/native/keyboard.ts
@@ -5,13 +5,13 @@ import deprecated from 'deprecated-decorator';
 const METHODS = ['hideKeyboardAccessoryBar', 'show', 'close', 'disableScroll', 'onKeyboardShow', 'onKeyboardHide'];
 
 export class KeyboardMock extends BaseMock {
-    constructor(){
+    constructor() {
         super('Keyboard', METHODS);
         this.setReturn('onKeyboardShow', ArrayObservable.of(undefined));
         this.setReturn('onKeyboardHide', ArrayObservable.of(undefined));
     }
 
-    @deprecated('new KeybaordMock()')
+    @deprecated('new KeyboardMock()')
     public static instance(): any {
         return new KeyboardMock();
     }
